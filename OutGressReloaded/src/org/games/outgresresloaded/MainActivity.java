51c7613fc50096835b47293957e936cdef824817
chispas-elek <elektro108@gmail.com>
2014-05-25 16:54:11 +0200

    Implementada la clase Login con todos los requerimientos necesarios

--- OutGressReloaded/src/org/games/outgresresloaded/MainActivity.java
+++ OutGressReloaded/src/org/games/outgresresloaded/MainActivity.java
@@ -1,5 +1,6 @@
 package org.games.outgresresloaded;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
@@ -8,12 +9,18 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,15 +35,20 @@ import android.preference.PreferenceManager;
 public class MainActivity extends Activity {
 
 	private JSONObject jsonObject;
+	private GoogleCloudMessaging gcm;
+	private static String SENDER_ID = "72974413367";
+	final SharedPreferences prefs = getSharedPreferences("preferenciasOR", Context.MODE_PRIVATE);
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//Comprobamos si existen datos del usuario guardados en el sistema
+		//Comprobamos si el dispositivo es compatible con GCM si lo es, que ejecute todo
+		//if(checkPlayServices()) {;
 		
-		final SharedPreferences prefs = getSharedPreferences("preferenciasOR", Context.MODE_PRIVATE);
-		String codigoUser = prefs.getString("codigo", "0");
+		
+		//Comprobamos si existen datos del usuario guardados en el sistema
+		String codigoUser = prefs.getString("validacion", "0");
 		if(codigoUser != "0") {
 			//El usuario se ha logueado previamente, comprobamos que esto sea así y saltamos a la pantalla principal
 			ArrayList<NameValuePair> parametros = new ArrayList<NameValuePair>();
@@ -46,10 +58,17 @@ public class MainActivity extends Activity {
 			try {
 				this.jsonObject = new JSONObject(result.get());
 				//Comprobamos si el identificador que teníamos guardado coincide con el de la BD
-				
+				int idUsuario = prefs.getInt("idusuario", 0);
+				if(jsonObject.getInt("idusuario") == idUsuario) {
+					//Los datos son correctos, asi que saltamos a la pantalla principal
+					
+				}else {
+					//Algo no ha ido bien
+					Log.e("Login", "Error a la hora de procesar los identificadores del login");
+				}
 			} catch (JSONException | InterruptedException | ExecutionException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
+				Log.e("Error JSON", "Error al procesar el archivo JSON");
 			}
 			
 			
@@ -65,14 +84,107 @@ public class MainActivity extends Activity {
 					//Preparamos los datos y llamamos a la BD
 					ArrayList<NameValuePair> parametros2 = new ArrayList<NameValuePair>();
 					parametros2.add(new BasicNameValuePair("usuario", usu.getText().toString()));
+					parametros2.add(new BasicNameValuePair("pass", pass.getText().toString()));
+					CumplePeticiones result2 = (CumplePeticiones) new CumplePeticiones(MainActivity.this,parametros2,"login.php").execute();
+					//Recogemos la respuesta
+					try {
+						jsonObject = new JSONObject(result2.get());
+						//Comprobamos que hayamos recibido algun dato
+						if(jsonObject.getString("usuario") != null) {
+							//El usuario logueado es correcto asi que procedemos a registrar los datos en el dispotivo.
+							//Registramos el dispositivo en el GCM y guardamos el identificador
+							registrarse();
+							//Guardaremos el idusuario y la validacion
+							SharedPreferences.Editor editor = prefs.edit();
+							editor.putInt("idusuario", jsonObject.getInt("idusuario"));
+							editor.putString("validacion", jsonObject.getString("validacion"));
+							editor.commit();
+							//Una vez validado el usuario se carga la interfaz principal del sistema
+							Toast.makeText(getApplicationContext(), "Login correcto, bienvenido", Toast.LENGTH_LONG).show();
+							
+							/*
+							 * Aqui se incluyen los datos necesarios para cargar la interfaz del usuario.
+							 * 
+							 */
+							
+						}else {
+							//Los datos introducidos son incorrectos
+							Toast.makeText(getApplicationContext(), "Los datos introducidos son incorrectos", Toast.LENGTH_LONG).show();
+						}
+					} catch (JSONException | InterruptedException
+							| ExecutionException e) {
+						e.printStackTrace();
+						Log.e("Error JSON","Error al manejar el JSON");
+					}
 					
-					
-					SharedPreferences.Editor editor = prefs.edit();
-					editor.putString("codigo", usu.getText().toString());
-					editor.commit();
 				}
 			});
 		}
+	}
+	
+	
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		//Comprobamos que nuestro dispositivo sea compatible con GCM
+		this.checkPlayServices();
+	}
+
+	/**
+	 * Ésta clase sirve para comprobar si el teléfono móvil soporta Google play services y si los tiene activados
+	 * @return True o False dependiendo de si tiene o no los Google Play Services
+	 */
+
+	private boolean checkPlayServices() {
+		 int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		 if (resultCode != ConnectionResult.SUCCESS) {
+			 if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+				 //Dispositivo no configurado. Mostrar ventana de configuración de Google Play Services
+				 //PLAY_SERVICES_RESOLUTION_REQUEST debe valer 9000
+				 GooglePlayServicesUtil.getErrorDialog(resultCode,this,9000).show();
+			 }else {
+				 //Dispositivo no compatible. Terminar la aplicación
+				 Log.i("Google Play Services", "This device is not supported.");
+				 finish(); 
+			 }
+			 return false;
+		 }
+		 return true;
+	}
+	
+	/**
+	 * En ésta clase se registra un dispositivo en el GCM
+	 * 
+	 */
+	
+	
+	private void registrarse() { 
+		new AsyncTask<Void,Void,String>(){ 
+			@Override
+			protected String doInBackground(Void... params) {
+				String msg = "";
+				try {
+					gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
+					//SENDER_ID contiene el número de registro del proyecto.
+					String regid = gcm.register(SENDER_ID);
+					msg  = "Dispositivo registrado correctamente con el regid: "+regid;
+					//Guardamos el GCM en las preferencias
+					SharedPreferences.Editor editor = prefs.edit();
+					editor.putString("gcm", regid);
+					editor.commit();
+				} catch (IOException ex) {
+					msg = "Error :" + ex.getMessage();
+				}
+				return msg;
+			}
+			@Override
+			protected void onPostExecute(String msg) {
+				//Mostramos un toast con lo que ha sucedido a la hora de registrar el dispositivo
+				Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+			}
+		}.execute(null, null, null);
+		
 	}
 }
 
